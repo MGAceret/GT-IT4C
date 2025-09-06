c1b4007a4d6443e272e286875308aaf9edc1b067
MGAceret <mgaceret@gmail.com>
2025-09-06 15:48:47 +0800

    Modified post.routes.js

--- express-seperation-of-concerns-lab/src/routes/post.routes.js
+++ express-seperation-of-concerns-lab/src/routes/post.routes.js
@@ -8,6 +8,10 @@ router.get('/', postController.getAllPosts);
 router.post('/', postController.createPost);
 router.get('/:id', postController.getPostById);
 router.put('/:id', postController.updatePost);
+
+// *** NEW ROUTE DEFINITION FOR PATCH ***
+router.patch('/:id', postController.partiallyUpdatePost);
+
 router.delete('/:id', postController.deletePost);
 
 export default router;
