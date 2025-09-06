1d4b5d844752ff3929001fb728e255e822d69109
MGAceret <mgaceret@gmail.com>
2025-09-06 15:54:21 +0800

    Updated app.js file

--- express-seperation-of-concerns-lab/app.js
+++ express-seperation-of-concerns-lab/app.js
@@ -1,6 +1,9 @@
 // app.js
 import express from 'express';
 import postRoutes from './src/routes/post.routes.js';
+// *** IMPORT THE NEW COMMENT ROUTES ***
+import commentRoutes from './src/routes/comment.routes.js';
+
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,9 @@ app.use(express.json());
 
 // Mount the post routes
 app.use('/posts', postRoutes);
+
+// *** MOUNT THE NEW COMMENT ROUTES ***
+app.use('/comments', commentRoutes);
 
 // In-memory "database"
 let posts = [
