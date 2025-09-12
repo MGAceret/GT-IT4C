a511fd5fa301fa317f300815b36664a141901789
MGAceret <mgaceret@gmail.com>
2025-09-12 23:05:51 +0800

    Added code for ApiError

---
+++ express-seperation-of-concerns-lab/src/utils/ApiError.js
@@ -0,0 +1,10 @@
+// src/utils/ApiError.js
+class ApiError extends Error {
+    constructor(statusCode, message) {
+        super(message); // Call the parent class (Error) constructor
+        this.statusCode = statusCode;
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+export default ApiError;
