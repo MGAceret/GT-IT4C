8998a537f1d414a65b7e857e56e9b091893f2804
Xyans Yes <mgaceret@gmail.com>
2025-09-21 02:21:27 +0800

    Added code

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+import express from 'express';
+import postRoutes from './src/routes/post.routes.js';
+import { testConnection } from './src/config/db.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.use('/posts', postRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    testConnection();
+});
