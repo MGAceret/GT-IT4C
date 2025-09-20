ce64ef8726a5e4d7174d4d4748eb1ba69b438386
Xyans Yes <mgaceret@gmail.com>
2025-09-21 02:34:41 +0800

    Added functionalities

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // index.js
 import express from 'express';
 import postRoutes from './src/routes/post.routes.js';
-import { testConnection } from './src/config/db.js';
+import { testConnection } from './src/config/db.js'; 
+import { errorHandler } from './src/middlleware/errorHandler.middleware.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-
+// Mount the post routes
 app.use('/posts', postRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-    testConnection();
+    testConnection(); // Test the database connection on startup
 });
