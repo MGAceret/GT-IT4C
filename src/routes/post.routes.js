495193d402f584b87e6c56eb663b518dac05a9cd
Xyans Yes <mgaceret@gmail.com>
2025-09-21 02:20:18 +0800

    Added routes

---
+++ src/routes/post.routes.js
@@ -0,0 +1,20 @@
+// src/routes/post.routes.js
+import { Router } from 'express';
+import * as postController from '../controllers/post.controller.js';
+
+const router = Router();
+
+router.post('/', validatePost, postController.createPost);
+router.put('/:id', validatePost, postController.updatePost);
+router.patch('/:id', postController.partiallyUpdatePost);
+
+// Apply the rules as middleware to the POST route
+router.post('/', createPostRules, postController.createPost);
+
+router.get('/', postController.getAllPosts);
+router.get('/:id', postController.getPostById);
+/*router.put('/:id', postController.updatePost); */
+router.put('/:id', updatePostRules, postController.updatePost);
+router.delete('/:id', postController.deletePost);
+
+export default router;
