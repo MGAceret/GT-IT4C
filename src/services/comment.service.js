13863c81abba83c498566fdd283ae17a6ecd9b88
MGAceret <mgaceret@gmail.com>
2025-09-21 21:07:50 +0800

    Added authorId as constraint for creating comment

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,15 +1,8 @@
 // src/services/comment.service.js
-
-// In-memory "database" for comments
-let comments = [
-    { id: 1, text: 'Great first post!', postId: 1 },
-    { id: 2, text: 'I agree, very insightful.', postId: 1 },
-    { id: 3, text: 'This is a comment on the second post.', postId: 2 },
-];
-let nextId = 4;
-
-// We need access to posts to ensure a post exists before adding a comment
 import { getPostById } from './post.service.js';
+import { getUserById } from './user.service.js'
+
+let nextId = 1;
 
 export const getAllComments = () => {
     return comments;
@@ -20,11 +13,17 @@ export const getCommentsByPostId = (postId) => {
 };
 
 export const createComment = (postId, commentData) => {
-    // Check if the post actually exists before creating a comment for it
+    // Validate whether the post exists
     const post = getPostById(postId);
     if (!post) {
-        return null; // Or throw an error
+        throw error;
     }
+
+    const user = getUserById(authorId);
+    if (!user) {
+        throw error;
+    }
+    
     const newComment = { id: nextId++, postId, ...commentData };
     comments.push(newComment);
     return newComment;
