f85e57a113a605042bedc9342da38798e33ecf38
MGAceret <mgaceret@gmail.com>
2025-10-18 21:52:52 +0800

    Modfied user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,7 @@ import * as userController from "../controllers/user.controller.js"
 
 const router = Router();
 
-router.post('/', userController.createUser);
+// router.post('/', userController.createUser);
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.get('/:userId/posts', userController.getPostsByUser);
