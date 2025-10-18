3a425ad14c1d562f4b498d8730f4e9de77f5ca33
MGAceret <mgaceret@gmail.com>
2025-10-19 03:08:37 +0800

    Added loginUser

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,4 +9,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json(new ApiResponse(201, newUser, "User registered successfully"));
 });
 
-// We will add a loginUser function here in the next lab
+export const loginUser = asyncHandler(async (req, res) => {
+    const token = await userService.loginUser(req.body);
+    res.status(200).json(new ApiResponse(200, { token }, "User logged in successfully!"));
+});
