7498af40ac63db44ed8bddcf6e5f292673655564
MGAceret <mgaceret@gmail.com>
2025-10-19 03:29:09 +0800

    Debug

--- src/middleware/validator.middleware.js
+++ src/middleware/validator.middleware.js
@@ -61,7 +61,25 @@ export const validateRegistration = [
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ success: false, errors: error.array() });
+            return res.status(400).json({ success: false, errors: errors.array() });
+        }
+        next();
+    }
+];
+
+export const validateLogin = [
+    body('email')
+        .isEmail()
+        .withMessage('A valid email is required.'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required.'),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
         }
         next();
     }
